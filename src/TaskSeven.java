23fb27c75477e7789c0a89efe961234487e13453
evgeniyshilov1111 <evgeniyshilov1111@gmail.com>
2025-02-02 12:17:19 +0300

    tasks

--- src/TaskSeven.java
+++ src/TaskSeven.java
@@ -2,7 +2,7 @@ public class TaskSeven {
     public static void main(String[] args) {
         //todo 7.	Дано целое число A. Проверить истинность высказывания: «Число A является положительным».
         int a = 40;
-        if (a >= 40) {
+        if (a >= 0) {
             System.out.println("Число A является положительным");
         } else {
             System.out.println("Число A не является положительным");
